90f9cd7cfe6291b2bed215d2cd046cdf8cfb1e51
Lito <lito@eordes.com>
2015-02-26 01:35:27 +0100

    Added untracked files

---
+++ src/anavallasuiza/laravel-Admin/Library/helpers.php
@@ -0,0 +1,30 @@
+<?php
+function query($key, $value = null)
+{
+    parse_str(parse_url(getenv('REQUEST_URI'), PHP_URL_QUERY), $query);
+
+    if (is_array($key)) {
+        $query = array_merge($query, $key);
+    } else {
+        $query[$key] = $value;
+    }
+
+    foreach ($query as $key => $value) {
+        if (strlen($value) === 0) {
+            unset($query[$key]);
+        }
+    }
+
+    return '?'.http_build_query($query);
+}
+
+function slug($string)
+{
+    $string = preg_replace('/[^\p{L}0-9]/u', '-', trim(strip_tags($string)));
+    $string = htmlentities($string, ENT_NOQUOTES, 'UTF-8');
+    $string = preg_replace('/&(\w)\w+;/', '$1', $string);
+    $string = preg_replace(['/\W/', '/\-+/'], '-', $string);
+    $string = preg_replace('/^\-|\-$/', '', $string);
+
+    return strtolower($string);
+}
---
+++ src/anavallasuiza/laravel-Admin/resources/assets/js/gettext.js
@@ -0,0 +1,106 @@
+$(function() {
+    'use strict';
+
+    var $gettextForm = $('#form-gettext'),
+        $gettextGroups = $gettextForm.find('.tab-pane > .gettext-group'),
+        $gettextSearch = $gettextForm.find('input[type="search"]'),
+        $gettextEmpty = $gettextForm.find('[data-gettext="empty"]'),
+        $gettextAdmin = $gettextForm.find('[data-gettext="admin"]'),
+        $gettextWeb = $gettextForm.find('[data-gettext="web"]'),
+        common = /\/(libs|models)\//;
+
+    if ($gettextForm.length === 0) {
+        return;
+    }
+
+    var gettextSearch = function ($group, text) {
+        var minChar = 3,
+            search = $gettextSearch.val().toLowerCase();
+
+        if (search.length < minChar) {
+            return true;
+        }
+
+        text = ($group.find('input').val() + $group.find('label').text()).toLowerCase();
+
+        return (text.indexOf(search) !== -1);
+    };
+
+    var gettextAdminShow = function ($group, text) {
+        return (!$gettextAdmin.is(':checked') || (text.indexOf('/admin/') !== -1) || text.match(common));
+    };
+
+    var gettextWebShow = function ($group, text) {
+        return (!$gettextWeb.is(':checked') || (text.indexOf('/admin/') === -1));
+    };
+
+    var gettextEmptyShow = function ($group, text) {
+        return (!$gettextEmpty.is(':checked') || ($group.find('input').val() === ''));
+    };
+
+    var gettextFilter = function () {
+        $gettextGroups.hide();
+
+        $gettextGroups.each(function () {
+            var $group = $(this),
+                text = $group.text().toLowerCase();
+
+            if (gettextSearch($group, text)
+            && gettextEmptyShow($group, text)
+            && gettextAdminShow($group, text)
+            && gettextWebShow($group, text)) {
+                $group.show();
+            }
+        });
+    }
+
+    $gettextSearch.on('keydown', function (e) {
+        var $this = $(this);
+
+        if (e.keyCode === 27) {
+            e.preventDefault();
+
+            $this.val('');
+            $gettextGroups.show();
+
+            return false;
+        }
+
+        if ((e.keyCode === 13) || (e.keyCode === 40)) {
+            e.preventDefault();
+            return false;
+        }
+
+        setTimeout(function () {
+            gettextFilter();
+        }, 500);
+    });
+
+    $gettextForm.find('[data-gettext]').on('change', function () {
+        gettextFilter();
+    });
+
+    $gettextGroups.find('label').on('dblclick', function () {
+        var $this = $(this);
+        $this.siblings('input').val($this.text());
+    });
+
+    $gettextGroups.find('.show-references').on('click', function (e) {
+        e.preventDefault();
+        $(this).siblings('.references').slideToggle();
+    });
+
+    $gettextForm.find('input[id^="entry-"]').on('change', function (e) {
+        $gettextForm.data('changed', true);
+    });
+
+    $gettextForm.on('submit', function () {
+        $gettextForm.data('changed', false);
+    });
+
+    window.onbeforeunload = function () {
+        if ($gettextForm.data('changed')) {
+            return gettext['gettext-changes'];
+        }
+    };
+});
