7e204155bc2ef6968c15f43b05354c26586a8410
Lito <lito@eordes.com>
2015-06-19 22:55:47 +0200

    Automated Controllers and Processors

---
+++ src/anavallasuiza/laravel-Admin/Http/Processors/Database/EditTrait.php
@@ -0,0 +1,112 @@
+<?php
+namespace Admin\Http\Processors\Database;
+
+use Exception;
+
+trait EditTrait
+{
+    public function duplicate($form, $row)
+    {
+        unset($_POST['id']);
+
+        $form['id']->val(0);
+
+        return $this->edit($form, self::getModel());
+    }
+
+    public function editCommon($form, $row)
+    {
+        if (!($data = $this->check(['edit', 'duplicate'], $form))) {
+            return $data;
+        }
+
+        self::checkDuplicates($data);
+
+        $previous = clone $row;
+
+        self::startFiles($form, $data);
+
+        try {
+            $row = self::getModel()->replace($data, $row, true);
+        } catch (Exception $e) {
+            throw new Exception(__('Error storing data: %s', $e->getMessage()));
+        }
+
+        self::endFiles($form, $data, $previous);
+
+        session()->flash('flash-message', [
+            'message' => __('Data was saved successfully'),
+            'status' => 'success',
+        ]);
+
+        return $row;
+    }
+
+    public function deleteCommon($form, $row)
+    {
+        if (!($data = $this->check('delete'))) {
+            return $data;
+        }
+
+        self::deleteFiles($row);
+
+        $row->delete();
+
+        session()->flash('flash-message', [
+            'message' => __('Data was saved successfully'),
+            'status' => 'success',
+        ]);
+
+        return $row;
+    }
+
+    private static function checkDuplicates(array $data)
+    {
+        if (empty(self::$duplicates)) {
+            return null;
+        }
+
+        $exists = self::getModel();
+
+        foreach (self::$duplicates as $column) {
+            $exists = $exists->orWhere($column, $data[$column]);
+        }
+
+        if ($data['id']) {
+            $exists = $exists->where('id', '!=', $data['id']);
+        }
+
+        if ($exists->first()) {
+            throw new Exception(__('Already exists another content with same %s', implode(__(' or '), self::$duplicates)));
+        }
+    }
+
+    private static function startFiles($form, array &$data)
+    {
+        if (empty(self::$files)) {
+            return null;
+        }
+
+        self::saveFormFiles($form, $data, self::getClass());
+    }
+
+    private static function endFiles($form, $data, $previous)
+    {
+        if (empty(self::$files) || empty($data['id'])) {
+            return null;
+        }
+
+        self::deleteOldFiles($form, $previous);
+    }
+
+    private static function deleteFiles($row)
+    {
+        if (empty(self::$files)) {
+            return null;
+        }
+
+        foreach (self::$files as $file) {
+            self::deleteFile($row->$file);
+        }
+    }
+}
---
+++ src/anavallasuiza/laravel-Admin/Library/DatabaseTrait.php
@@ -0,0 +1,41 @@
+<?php
+namespace Admin\Library;
+
+use App, Auth;
+
+trait DatabaseTrait
+{
+    protected static function getClass()
+    {
+        return class_basename(__CLASS__);
+    }
+
+    protected static function getModel($model = null)
+    {
+        $model = $model ?: class_basename(__CLASS__);
+
+        return App::make('App\Models\\'.$model);
+    }
+
+    protected static function getForm($function, $form = null)
+    {
+        if (is_object($form)) {
+            return $form;
+        }
+
+        $form = $form ?: class_basename(__CLASS__);
+
+        return App::make('Admin\Http\Controllers\Database\Forms\\'.$form)->$function();
+    }
+
+    protected static function getRow($row = null)
+    {
+        if (is_object($row)) {
+            return $row;
+        }
+
+        $model = self::getModel();
+
+        return $row ? $model->where('id', (int)$row)->firstOrFail() : $model;
+    }
+}
